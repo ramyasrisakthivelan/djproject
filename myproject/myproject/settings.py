b6a9bb271c5dd18504c0db3640bc838f57f0b12e
Ashok <137053596+ashokbell@users.noreply.github.com>
2023-07-02 21:31:58 +0530

    update file

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -15,8 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR=os.path.join(BASE_DIR,'template')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+
+
 
 
 
@@ -120,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR=[STATIC_DIR,
-                 ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 
+]
+  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
